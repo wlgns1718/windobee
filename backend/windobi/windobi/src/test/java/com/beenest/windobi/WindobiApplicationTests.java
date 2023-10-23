a7b8a436aadea808a9acbc950fbee2578f0c305d
jeonghwanKim <doyun30@naver.com>
2023-10-23 14:06:41 +0900

    init

---
+++ backend/windobi/windobi/src/test/java/com/beenest/windobi/WindobiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beenest.windobi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WindobiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
