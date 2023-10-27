1a81379d2340c52eb8c17cb985ac06dc0e32915e
DESKTOP-DOGVPUB\SSAFY <ssafy>
2023-10-27 17:32:57 +0900

    fix: conflict 해결
    Former-commit-id: cf696677a6f71dd9bf4046e333a74937bf0f34d2

---
+++ backend/windobi/src/main/java/com/beenest/windobi/user/controller/UserController.java
@@ -0,0 +1,21 @@
+package com.beenest.windobi.user.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/user")
+@Slf4j
+@RequiredArgsConstructor
+public class UserController {
+
+    @GetMapping
+
+
+}
