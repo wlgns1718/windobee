a1ecf32c81b4a088e87cf2b45bc558197ab2cd32
mincheolsong <thdalscjf05@naver.com>
2023-11-13 17:57:59 +0900

    Feat : 보고서 조금 수정
    Former-commit-id: fe995056d16a717240a8b5efd2f2b88a72f88ff9

--- electron/src/renderer/pages/CreatedChart.tsx
+++ electron/src/renderer/pages/CreatedChart.tsx
@@ -5,6 +5,8 @@ import BarChart from '../components/jobtime/BarChart';
 import { ipcRenderer } from 'electron';
 import { ResponsiveBar } from '@nivo/bar';
 import * as S from '../components/report/Report.style';
+import RecentApplication from '../components/jobtime/RecentApplication';
+import PieChart from '../components/jobtime/PieChart';
 
 type Data = {
   day: number;
@@ -32,7 +34,6 @@ function CreatedChart() {
   const timeAvgHour: number = Math.floor(timeAvg, 0);
   const timeAvgMin: number = Math.floor((timeAvg % 1) * 60, 0);
 
-
   return (
     <div ref={ref} style={{ width: '100%', height: '100%' }}>
       {/* <BarChart
@@ -45,6 +46,14 @@ function CreatedChart() {
         <div key={index}>{JSON.stringify(item)}</div>
       ))} */}
 
+      <div style={{ width: '200px', height: '200px' }}>
+        <PieChart
+          application="Visual Studio Code"
+          day={new Date()}
+          type="weekly"
+        ></PieChart>
+      </div>
+      <RecentApplication></RecentApplication>
       <div>
         <S.header>
           <S.bolder>{timeAvgHour}</S.bolder>시간{' '}
@@ -52,110 +61,34 @@ function CreatedChart() {
         </S.header>
         <S.lighter>하루 평균 사용시간</S.lighter>
       </div>
-      <ResponsiveBar
-        data={result}
-        keys={['time']}
-        indexBy="day"
-        margin={{ top: 50, right: 130, bottom: 100, left: 60 }}
-        padding={0.7}
-        valueScale={{ type: 'linear' }}
-        indexScale={{ type: 'band', round: true }}
-        colors={['#2694F5']}
-        enableLabel={false}
-        defs={[
-          {
-            id: 'dots',
-            type: 'patternDots',
-            background: 'inherit',
-            color: '#2694F5',
-            size: 4,
-            padding: 1,
-            stagger: true,
-          },
-          {
-            id: 'lines',
-            type: 'patternLines',
-            background: 'inherit',
-            color: '#2694F5',
-            rotation: -45,
-            lineWidth: 6,
-            spacing: 10,
-          },
-        ]}
-        // fill={[
-        //   {
-        //     match: {
-        //       id: 'fries',
-        //     },
-        //     id: 'dots',
-        //   },
-        //   {
-        //     match: {
-        //       id: 'sandwich',
-        //     },
-        //     id: 'lines',
-        //   },
-        // ]}
-        borderColor={{
-          from: 'color',
-          modifiers: [['darker', 1.6]],
-        }}
-        borderRadius={'10px'}
-        axisTop={null}
-        axisRight={null}
-        // axisBottom={{
-        //   tickSize: 5,
-        //   tickPadding: 5,
-        //   tickRotation: 0,
-        //   legend: 'country',
-        //   legendPosition: 'middle',
-        //   legendOffset: 32,
-        // }}
-        // axisLeft={{
-        //   tickSize: 5,
-        //   tickPadding: 5,
-        //   tickRotation: 0,
-        //   legend: 'food',
-        //   legendPosition: 'middle',
-        //   legendOffset: -40,
-        //   tickValues: [0, 2, 4, 6],
-        // }}
-        labelSkipWidth={12}
-        labelSkipHeight={12}
-        labelTextColor={{
-          from: 'color',
-          modifiers: [['darker', 1.6]],
-        }}
-        // legends={[
-        //   {
-        //     dataFrom: 'keys',
-        //     anchor: 'bottom-right',
-        //     direction: 'column',
-        //     justify: false,
-        //     translateX: 120,
-        //     translateY: 0,
-        //     itemsSpacing: 2,
-        //     itemWidth: 100,
-        //     itemHeight: 20,
-        //     itemDirection: 'left-to-right',
-        //     itemOpacity: 0.85,
-        //     symbolSize: 20,
-        //     effects: [
-        //       {
-        //         on: 'hover',
-        //         style: {
-        //           itemOpacity: 1,
-        //         },
-        //       },
-        //     ],
-        //   },
-        // ]}
-        role="application"
-        ariaLabel="Nivo bar chart demo"
-        // barAriaLabel={(e) =>
-        //   e.id + ': ' + e.formattedValue + ' in country: ' + e.indexValue
-        // }
-      />
+      <div style={{ height: '400px' }}>
+        <ResponsiveBar
+          data={result}
+          keys={['time']}
+          indexBy="day"
+          margin={{ top: 50, right: 130, bottom: 100, left: 60 }}
+          padding={0.7}
+          valueScale={{ type: 'linear' }}
+          indexScale={{ type: 'band', round: true }}
+          colors={['#2694F5']}
+          enableLabel={false}
+          borderColor={{
+            from: 'color',
+            modifiers: [['darker', 1.6]],
+          }}
+          borderRadius={'10px'}
+          axisTop={null}
+          axisRight={null}
+          labelSkipWidth={12}
+          labelSkipHeight={12}
+          labelTextColor={{
+            from: 'color',
+            modifiers: [['darker', 1.6]],
+          }}
+          role="application"
+          ariaLabel="Nivo bar chart demo"
+        />
+      </div>
     </div>
   );
 }
